1b8f85a601417413cdcf2f87d4d72a6c447d2698
Maxime Culea <culea.maxime@gmail.com>
2018-05-22 10:12:51 +0200

    #6 : fix js for title display

--- assets/js/bea-beautiful-flexible.js
+++ assets/js/bea-beautiful-flexible.js
@@ -1,40 +1,23 @@
+// Delete the ACF Beautiful Flexible popup if only one layout
 jQuery(document).ready(function ($) {
-    try {
-        // ACF Flexible Content: Ajouter une class à la modal de sélection de Layouts
-        var flexible_content_position = acf.fields.flexible_content.position_popup;
-        acf.fields.flexible_content.position_popup = function ($popup, $bouton) {
-            // Continuer l'exécution normalement
-            return flexible_content_position.apply(this, arguments);
+    var flexible_content_open = acf.fields.flexible_content._open;
+    acf.fields.flexible_content._open = function (e) {
+        var $popup = $(this.$el.children('.tmpl-popup').html());
+        if ($popup.find('a').length == 1) {
+            // Only one layout
+            acf.fields.flexible_content.add($popup.find('a').attr('data-layout'));
+            return false;
         }
-    } catch (e) {
+        return flexible_content_open.apply(this, arguments);
     }
 });
 
-jQuery(document).ready(function ($) {
-    try {
-        // ACF Flexible Content: Supprimer la popup si il n'y a qu'un seul Layout
-        var flexible_content_open = acf.fields.flexible_content._open;
-        acf.fields.flexible_content._open = function (e) {
-            var $popup = $(this.$el.children('.tmpl-popup').html());
-            // On compte le nombre de layouts
-            if ($popup.find('a').length == 1) {
-                acf.fields.flexible_content.add($popup.find('a').attr('data-layout'));
-                return false;
-            }
-            // Si plus d'un layout, continuer l'exécution normalement
-            return flexible_content_open.apply(this, arguments);
-        }
-    } catch (e) {
-    }
-});
-
-jQuery(document).ready(function ($) {
-    $('a[data-name="add-layout"]').on('click', function () {
-        setTimeout(function () {
-            $(".acf-fc-popup a").each(function () {
-                var html = "<div>" + $(this).text() + "</div>";
-                $(this).html(html);
-            });
-        }, 0);
-    });
+// Transform a link into a div for styling purpose
+jQuery('body').on('click', 'a[data-name="add-layout"]', function () {
+    setTimeout(function () {
+        $(".acf-fc-popup a").each(function () {
+            var html = "<div>" + $(this).text() + "</div>";
+            $(this).html(html);
+        });
+    }, 0);
 });
