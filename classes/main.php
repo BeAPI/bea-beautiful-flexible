e6df9142ef11476209f458d50385996d3b3a2726
Maxime Culea <culea.maxime@gmail.com>
2018-10-17 16:37:54 +0200

    #10 : fix the empty check as it is an array and not just a key

--- classes/main.php
+++ classes/main.php
@@ -65,7 +65,7 @@ class Main {
 				if ( 'flexible_content' === $field['type'] ) {
 					// Flexible is recursive structure with sub_fields into layouts
 					foreach ( $field['layouts'] as $layout_field ) {
-						if ( ! empty( $keys [ $layout_field ] ) ) {
+						if ( ! empty( $keys [ $layout_field['key'] ] ) ) {
 							continue;
 						}
 						$keys[ $layout_field['key'] ] = $layout_field['name'];
