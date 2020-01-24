3a1d7cedffa3f6b46d264fd2bef86c9f04d12998
Nicolas Juen <njuen@beapi.fr>
2020-01-24 17:37:03 +0100

    Fix issue #16

--- assets/js/bea-beautiful-flexible-57.js
+++ assets/js/bea-beautiful-flexible-57.js
@@ -1,8 +1,8 @@
 // Delete the ACF Beautiful Flexible popup if only one layout
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function (jQuery) {
     var flexible_content_open = acf.getField( 'acf-field-flexible-content' );
     flexible_content_open._open = function (e) {
-        var $popup = $(this.$el.children('.tmpl-popup').html());
+        var $popup = jQuery(this.$el.children('.tmpl-popup').html());
         if ($popup.find('a').length == 1) {
             // Only one layout
             flexible_content_open.add($popup.find('a').attr('data-layout'));
@@ -15,9 +15,9 @@ jQuery(document).ready(function ($) {
 // Transform a link into a div for styling purpose
 jQuery('body').on('click', 'a[data-name="add-layout"]', function () {
     setTimeout(function () {
-        $(".acf-fc-popup a").each(function () {
-            var html = "<div>" + $(this).text() + "</div>";
-            $(this).html(html);
+        jQuery(".acf-fc-popup a").each(function () {
+            var html = "<div>" + jQuery(this).text() + "</div>";
+            jQuery(this).html(html);
         });
     }, 0);
 });
--- assets/js/bea-beautiful-flexible.js
+++ assets/js/bea-beautiful-flexible.js
@@ -1,8 +1,8 @@
 // Delete the ACF Beautiful Flexible popup if only one layout
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function () {
     var flexible_content_open = acf.fields.flexible_content._open;
     acf.fields.flexible_content._open = function (e) {
-        var $popup = $(this.$el.children('.tmpl-popup').html());
+        var $popup = jQuery(this.$el.children('.tmpl-popup').html());
         if ($popup.find('a').length == 1) {
             // Only one layout
             acf.fields.flexible_content.add($popup.find('a').attr('data-layout'));
@@ -15,9 +15,9 @@ jQuery(document).ready(function ($) {
 // Transform a link into a div for styling purpose
 jQuery('body').on('click', 'a[data-name="add-layout"]', function () {
     setTimeout(function () {
-        $(".acf-fc-popup a").each(function () {
-            var html = "<div>" + $(this).text() + "</div>";
-            $(this).html(html);
+        jQuery(".acf-fc-popup a").each(function () {
+            var html = "<div>" + jQuery(this).text() + "</div>";
+            jQuery(this).html(html);
         });
     }, 0);
 });
